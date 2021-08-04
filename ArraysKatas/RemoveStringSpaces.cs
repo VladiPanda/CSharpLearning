793bff8a66b5f11ef480237e5240903f39f01cc2
Vladimir Trofimov <trofimov57@mail.ru>
2021-08-04 15:03:41 +0300

    2. Remove String Spaces

---
+++ ArraysKatas/RemoveStringSpaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ArraysKatas
+{
+    // Simple, remove the spaces from the string, then return the resultant string.
+    public class RemoveStringSpaces
+    {
+        public string NoSpace(string input)
+        {
+            return input.Replace(" ", "");
+        }  
+    }
+}
