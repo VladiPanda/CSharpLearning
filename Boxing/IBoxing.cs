742919a880ddb58f4d62e941baec8fd0a5403cb4
Vladimir Trofimov <trofimov57@mail.ru>
2021-08-19 18:14:06 +0300

    Boxing, Enums, DataTypes, ArrayCovariant

---
+++ Boxing/IBoxing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Boxing
+{
+    public interface IBoxing
+    {
+        
+    }
+
+    public interface IInterface
+    {
+        void Method();
+    }
+    
+    struct MyStruct2 : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+}
---
+++ EnumsSimpleCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EnumsSimpleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+
+            int keyCode = (int) key;
+
+            Console.WriteLine($"\tEnum {key}\tKey Code{keyCode}");
+
+            if (key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Вы нажали 'Enter'!");
+            }
+        }
+    }
+}
