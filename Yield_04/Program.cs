36e826b52eda518fc4d7a6b8728a082050f01f0b
Vladimir Trofimov <trofimov57@mail.ru>
2021-08-30 12:29:58 +0300

    Collections, Yield

---
+++ Yield_04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Yield_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.Write(element);
+
+            // Delay. 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Yield_05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Yield_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.WriteLine(element);
+
+            // Delay. 
+            Console.ReadKey();
+        }
+    }
+}
