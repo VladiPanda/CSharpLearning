6a247e3d0f08e99b0a63f97e5dd3432208ab903a
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-05 16:02:57 +0300

    Class Object, Implicit, Explicit, ICloneable, MemberwiseClone, Override Operators

---
+++ ICloneable/Point.cs
@@ -0,0 +1,30 @@
+﻿namespace ICloneable
+{
+    // Глубокое копиирование (Deep copy)
+
+    public class Point : System.ICloneable
+    {
+        public int x, y;
+
+        public Point()
+        {
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Реализация метода интерфейса ICloneable.
+        public object Clone()
+        {
+            return new Point(this.x, this.y) as object;
+        }
+
+        public override string ToString()
+        {
+            return "X: " + x + " Y: " + y;
+        }
+    }
+}
