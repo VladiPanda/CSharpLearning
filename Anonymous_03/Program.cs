f448b9f4675c592abd7303ffa8bed72d3f1419a1
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-08 14:12:33 +0300

    Anonymous, LINQ, Dynamics

---
+++ Anonymous_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Анонимные типы. (Сильная ссылка)
+
+namespace Anonymous_03
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var instance = new { My = new MyClass() };
+
+            instance.My.field = 1;
+            instance.My.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
