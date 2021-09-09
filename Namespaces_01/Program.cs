2a3ff3316acb9906857ca4970ce972461eb255b3
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-09 16:02:13 +0300

    Namespaces, Internal, Librarys, PreprocessorDirectives

---
+++ Namespaces_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пространства имен.
+
+// Директива using - импортирует пространство имен, избавляя от необходимости полной квалификации имен стереотипов.
+
+using System;
+using NamespaceA;
+
+// Закомментировать.
+
+namespace Namespaces_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1
+            Console.WriteLine("Hello world");
+            // 2
+            MyClass my = new MyClass();
+            // Полная квалификация имени класса, включает имя пространства имен, в котором находится класс.
+            NamespaceA.MyClass my2 = new NamespaceA.MyClass();
+        }
+    }
+}
---
+++ Namespaces_09/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пространства имен.
+
+namespace MyNamespace
+{
+    class MyClass { }
+
+    namespace MyNamespace
+    {
+        // Допустимо иметь во вложенных пространствах имен, одноименные стереотипы.
+
+        class MyClass { }
+    }
+}
+
+
+namespace Namespaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyNamespace.MyClass my1 = new MyNamespace.MyClass();
+            MyNamespace.MyNamespace.MyClass my2 = new MyNamespace.MyNamespace.MyClass();
+        }
+    }
+}
