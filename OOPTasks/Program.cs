28c3d6d500776eb6e43ba06ffb14195bceb1d89e
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-16 22:23:44 +0300

    Generics # 1

---
+++ OOPTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generics # 1
+            
+            // Создаем переменную типа i и присваеваем ей экземпляр который будет возвращать метод AddingMethod
+            int i = MyClass<int>.AddingMethod();
+            // Проверяем тип и имя
+            Console.WriteLine(i.GetType().Name);
+
+            Program p = MyClass<Program>.AddingMethod();
+            Console.WriteLine(p.GetType().Name);
+        }
+    }
+}
