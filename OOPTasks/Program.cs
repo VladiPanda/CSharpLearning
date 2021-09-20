502b5c980e9d2922d133fe0eae0890555880bf96
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-20 16:54:07 +0300

    Generics task 1

--- OOPTasks/Program.cs
+++ OOPTasks/Program.cs
@@ -8,13 +8,47 @@ namespace OOPTasks
         {
             // Generics # 1
             
-            // Создаем переменную типа i и присваеваем ей экземпляр который будет возвращать метод AddingMethod
+            // Создаем переменную типа i и присваиваем ей экземпляр который будет возвращать метод AddingMethod
             int i = MyClass<int>.AddingMethod();
             // Проверяем тип и имя
             Console.WriteLine(i.GetType().Name);
 
             Program p = MyClass<Program>.AddingMethod();
             Console.WriteLine(p.GetType().Name);
+            
+            // Generics # 2
+
+            Console.WriteLine("Type massive Length: ");
+            string str = Console.ReadLine();
+            int a = string.IsNullOrEmpty(str) ? 0 : Convert.ToInt32(str);
+
+            var list = new MyList<int>();
+
+            var b = new Random();
+            for (int x = 0; x < a; x++)
+            {
+                list.Add(b.Next(100));
+            }
+
+            Console.WriteLine("Massive elements: ");
+            for (int q = 0; q < list.Count; q++)
+            {
+                Console.Write(list[q] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine("Type value for searching: ");
+            if (list.Contains(Convert.ToInt32(Console.ReadLine())))
+            {
+                Console.WriteLine("Zbs, ono est'");
+            }
+            else
+            {
+                Console.WriteLine("Ne zbz, ego net");
+            }
+
         }
     }
 }
