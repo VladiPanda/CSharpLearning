5492a08185479d5ecf4b205f9c29728e6b90c188
Vladimir Trofimov <trofimov57@mail.ru>
2021-09-30 17:45:17 +0300

    SoloLearn

---
+++ SoloLearnWords/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoloLearnWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words =
+            {
+                "home", "programming", "victory", "C#", "football", "sport", "book", "learn", "dream", "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+
+            foreach (var word in words)
+            {
+                if (word.Contains(letter))
+                {
+                    Console.WriteLine(word);
+                    count++;
+                }
+
+                if (count == 0)
+                {
+                    Console.WriteLine("No match");
+                    break;
+                }
+            }
+        }
+    }
+}
+    
