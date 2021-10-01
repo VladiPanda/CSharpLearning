228f55943d2d38ac5d4e0c0873941b812a8deca0
Vladimir Trofimov <trofimov57@mail.ru>
2021-10-01 21:08:02 +0300

    Stopwatch example

---
+++ StopwatchWPFtest/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace StopwatchWPFtest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string myobject;   // Название объектов-кнопок в таймере
+        DispatcherTimer timer;  // Создание экземпляра класса DispatcherTimer, который отвечает за таймер (периодическое повторение определенных действий над элементами программы) и
+                                //     обрабатывается в течение указанного интервала времени
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Presenter(this);
+            timer = new DispatcherTimer();  
+            timer.Interval = TimeSpan.FromMilliseconds(0.001);  // Период времени между тактами (в 1 миллисекунду)
+            timer.Tick += Timer_Tick; // Событие, которое возникает при истечении интервала таймера (повторно вызывает метод-обработчик).
+        }
+
+        public event EventHandler EventStart = null;  // Событие, которое происходит в момент нажатия на кнопку Start
+        public event EventHandler EventReset = null;  // Событие, которое происходит в момент нажатия на кнопку Reset
+
+
+        // Метод-обработчик события Tick, которое реализует функционал запуска, остановки, обнуления счета таймера
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (sender.GetType().Name != "DispatcherTimer")  
+            { 
+             myobject = ((Button)sender).Name; 
+            }
+            switch (myobject) // Проверка на нажатую кнопку
+            {
+                case "Start":
+                    {
+                        timer.Start();  // Метод, который запускает таймер (запускает System.Windows.Threading.DispatcherTimer)
+                        EventStart.Invoke(sender, e);
+                        break;
+                    }
+                case "Stop":
+                    {
+                        timer.Stop();  // Метод, который останавливает таймер (запускает System.Windows.Threading.DispatcherTimer)
+                        break;
+                    }
+                case "Reset":
+                    {
+                        timer.Stop();
+                        EventReset.Invoke(sender, e);
+                        break;
+                    }
+                default:
+                    {
+                        timer.Stop();
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ StopwatchWPFtest/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StopwatchWPFtest
+{
+    class Model
+    {
+        public string AddNumber(string s)   // Метод, который инкрементирует значение счета таймера на одну единицу
+        {
+            int number = Convert.ToInt32(s);
+            
+            ++number;
+            s = Convert.ToString(number);
+            if (s.Length == 1)
+            {
+                s = "0" + s;
+            }
+            return s;
+        }
+    }
+}
