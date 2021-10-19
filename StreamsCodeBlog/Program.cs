0447d1310d695cd13b1d527610772c9a33c5ee71
Vladimir Trofimov <trofimov57@mail.ru>
2021-10-19 12:51:30 +0300

    StreamsCodeBlog

---
+++ StreamsCodeBlog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StreamsCodeBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter("test.txt", true, Encoding.UTF8))
+            {
+                sw.Write("Hello, world");
+                sw.WriteLine("Hello, user");
+            }
+
+            using (var sr = new StreamReader("test.txt"))
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
